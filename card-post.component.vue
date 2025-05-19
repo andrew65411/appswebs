3df6dba02039689f767e59ae0c30e77c16163046
andrew65411 <83627200+andrew65411@users.noreply.github.com>
2025-05-19 15:26:35 -0500

    Create card-post.component.vue

---
+++ card-post.component.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post-card">
+    <img :src="post.image" :alt="post.title" />
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.summary }}</p>
+    <button @click="$emit('see-more', post.id)">See More</button>
+  </div>
+</template>
+
+<script>
+import { Post } from '@/public/model/post.model';
+
+export default {
+  name: 'CardPost',
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post-card {
+  border: 1px solid #ddd;
+  padding: 1rem;
+  text-align: center;
+  margin: 1rem;
+}
+
+.post-card img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
