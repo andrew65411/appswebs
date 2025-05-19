54cfe8fd2f5669f1d8b5f507c9a7f16fc5437e80
andrew65411 <83627200+andrew65411@users.noreply.github.com>
2025-05-19 15:26:02 -0500

    Create pv-toolbar.component.vue

---
+++ pv-toolbar.component.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pv-toolbar">
+    <img :src="logoUrl" alt="Food Truck Lovers Logo" width="40" />
+    <span>Food Truck Lovers</span>
+
+    <nav>
+      <router-link to="/home">Home</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PvToolbar',
+  data() {
+    return {
+      logoUrl: 'https://logo.clearbit.com/foodtrucklovers.io '
+    };
+  }
+};
+</script>
+
+<style scoped>
+.pv-toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #f8f9fa;
+  border-bottom: 1px solid #dee2e6;
+}
+
+nav {
+  display: flex;
+  gap: 1rem;
+}
+</style>
