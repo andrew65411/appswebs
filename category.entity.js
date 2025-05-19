f2ae4fa226d615dc6225144cad60c057b913ef57
andrew65411 <83627200+andrew65411@users.noreply.github.com>
2025-05-19 15:19:20 -0500

    Update category.entity.js

--- category.entity.js
+++ category.entity.js
@@ -1,1 +1,18 @@
-
+/**
+ * @class Category
+ * @description Represents a category for organizing events or content.
+ */
+export class Category {
+  /**
+   * @constructor
+   * @param {Object} data - Raw category data from API
+   * @param {string} data.id - Unique ID of the category
+   * @param {string} data.name - Name of the category
+   * @param {string} data.description - Description of the category
+   */
+  constructor({ id = '', name = '', description = '' }) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
+}
